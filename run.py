d2463c853f73819721228a0aa4c47b644f6a4599
Galaxy Master <Marcel.Hajek@gmx.de>
2025-06-20 12:08:47 +0200

    init

--- run.py
+++ run.py
@@ -1,33 +1,62 @@
 import os
 import platform
-import subprocess
+from subprocess import run, PIPE
 
-project_name: str = "file2str"
+project_name: str = "example"
+windows: bool = platform.system() == "Windows"
 
-if platform.system() == "Windows":
-	subprocess.run("rmdir /S /Q build", shell = True)
+if windows:
+	run("rmdir /S /Q build", shell = True)
 else:
-	subprocess.run("rm -rf build", shell = True)
+	run("rm -rf build", shell = True)
 
 if not os.path.exists("build"):
-	subprocess.run("mkdir build", shell=True)
+	run("mkdir build", shell=True)
 
-result = subprocess.run(
-	f"conan install . --build=missing -of build && cmake . -B build && make -C build",
-	check=True,
-	shell=True,
-	stdout=subprocess.PIPE,
-	stderr=subprocess.PIPE
-)
+def execute_cmd(cmd: str, description: str):
+	conan_result = run(
+		cmd,
+		shell=True,
+		stdout=PIPE,
+		stderr=PIPE
+	)
 
-if result.stdout:
-	print(result.stdout.decode())
-if result.stderr:
-	print(result.stderr.decode())
+	if conan_result.returncode == 0:
+		print(description + " was successful!!!\n\n\n")
+	else:
+		print("---DETAIL ZONE---")
+		if conan_result.stdout:
+			print(conan_result.stdout)
+		if conan_result.stderr:
+			print(conan_result.stderr)
+		print("---END DETAIL ZONE---")
+		
+		print("\n\n\n" + description + " has failed! (details are above)")
+		exit(1)
 
-if platform.system() == "Windows":
+
+
+cmds = [
+			(
+				"conan install . --build=missing -of build",
+				"Conan installing packages required by your conanfile.txt"
+			),
+			(
+				"cmake . -B build",
+				"CMake pre generating the stuff necessary for building your project"
+			),
+			(
+				"make -C build",
+				"Make trying to build your project form the cmake generated stuff"
+			)
+		]
+
+for cmd in cmds:
+	execute_cmd(cmd[0], cmd[1])
+
+if windows:
 	if os.path.exists(f".\\build\\{project_name}"):
-		subprocess.run([f".\\build\\{project_name}.exe"], check = True)
+		run([f".\\build\\{project_name}.exe"], check = True)
 else:
 	if os.path.exists(f"./build/{project_name}"):
-		subprocess.run([f"./build/{project_name}"], check = True)
+		run([f"./build/{project_name}"], check = True)
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,127 @@
 #include <iostream>
+#include <fstream>
+#include <expected>
+#include <string>
+#include <string_view>
+#include <optional>
 
-int main(int argc, const char* argv[])
+inline auto check_arg_count(const int argc) -> bool
 {
-	std::cout << "Hello World!\n";
+	return argc == 3;
+}
+
+auto try_fetch_content(const int argc, const char *argv[]) -> std::optional<std::string>
+{
+	if (check_arg_count(argc))
+	{
+		return std::nullopt;
+	}
+
+	std::string content;
+	std::ifstream stream;
+	stream.open(argv[1]);
+
+	stream >> content;
+
+	stream.close();
+	return std::move(content);
+};
+
+auto format_to_str(const std::string_view input_str) -> std::optional<std::string>
+{
+	if (not input_str.empty())
+	{
+
+		std::string result{"#pragma once\n#include <string_view>\n\nconstexpr std::string_view str = \""};
+		result.reserve(80);
+
+		for (const auto &c : input_str)
+		{
+			switch (c)
+			{
+			case '\n':
+				result.append("\\n");
+				break;
+			case '\"':
+				result.append("\\\"");
+				break;
+			case '\t':
+				result.append("\\t");
+				break;
+			case '\r':
+				result.append("\\r");
+				break;
+			default:
+				result.push_back(c);
+				break;
+			}
+		}
+
+		result.push_back('\";');
+
+		return std::move(result);
+	}
+
+	return std::nullopt;
+};
+
+/// @return if there is a str, it means, something went wrong
+auto save_data(
+	const int argc,
+	const char *argv[],
+	const std::string &str) -> std::optional<std::string_view>
+{
+	if (check_arg_count(argc))
+	{
+		std::ofstream stream;
+
+		try
+		{
+			stream.open(argv[2]);
+
+			stream << str;
+
+			stream.close();
+		}
+		catch (const std::exception &e)
+		{
+			return e.what();
+		}
+
+		return std::nullopt;
+	}
+
+	return "argument validation failed";
+};
+
+auto main(const int argc, const char *argv[]) -> int
+{
+	const auto &fetch_optional = try_fetch_content(argc, argv);
+
+	if (fetch_optional.has_value())
+	{
+		const auto &format_optional =
+			format_to_str(fetch_optional.value());
+
+		if (format_optional.has_value())
+		{
+			const auto &save_optional_error =
+				save_data(argc, argv, format_optional.value());
+
+			if (save_optional_error.has_value())
+			{
+				std::cout
+					<< "\n"
+					<< save_optional_error.value()
+					<< "\n";
+			}
+			else
+			{
+				std::cout
+					<< "success!\n";
+			}
+		}
+	}
 
 	return 0;
-}
+};
